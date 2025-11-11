68ca1c4f5e218abdb9ad7e160536d35c5e82b930
CreativeName1 <ben777w@gmail.com>
2025-11-11 14:47:34 -0500

    add train functionality

--- beam_functions.py
+++ beam_functions.py
@@ -10,6 +10,7 @@ class BeamModel:
         self.EI = EI
         self.R = [-1, -1]  # support reactions
         self.beam_analysis = None
+        self.bridge_env = None
 
     def add_support(self, distance, support_type):
 
@@ -46,6 +47,20 @@ class BeamModel:
         span = self.identify_span(distance)
         self.beam_analysis.add_udl(i_span=span, w=udl)
 
+
+    #car1_load represents the load of the lightest freight car in Load Configuration 2.
+    def create_train(self, car1_load):
+
+        locomotive = cba.vehicle.Vehicle([176], [car1_load*1.38/2]*2)
+        car_light = cba.vehicle.Vehicle([176], [car1_load/2]*2)
+        car_heavy = cba.vehicle.Vehicle([176], [car1_load*1.1/2]*2)
+
+        vehicles = [locomotive, car_light, car_heavy]
+        spacings = [164]*2
+        train = cba.vehicle.make_train(vehicles, spacings)
+
+        return train
+
     def analyze(self, n_points=10000):
         self.beam_analysis = cba.BeamAnalysis(self.L, self.EI, self.R, self.LM)
         self.beam_analysis.analyze(n_points)
@@ -53,7 +68,18 @@ class BeamModel:
         print("Max moment (Nm):", self.beam_analysis.beam_results.vRes[0].M.max())
         print("Max shear (N):", self.beam_analysis.beam_results.vRes[0].V.max())
 
+
+    #car1_load represents the load of the lightest freight car in Load Configuration 2.
+    def analyze_train(self, car1_load):
+        self.analyze()
+        bridge_analysis = cba.BridgeAnalysis(self.beam_analysis, self.create_train(car1_load))
+        self.bridge_env = bridge_analysis.run_vehicle(0.1)
+
     def display(self):
         if not self.beam_analysis:
             raise RuntimeError("Beam not analyzed yet.")
+        if not self.bridge_env:
+            raise RuntimeError("self.analyze_train(car1_load) must be called first")
         self.beam_analysis.plot_results()
+        self.bridge_env.plot()
+        plt.show()
--- test of beam_functions.py
+++ test of beam_functions.py
@@ -3,6 +3,5 @@ from beam_functions import BeamModel
 beam = BeamModel()
 beam.add_support(3, "pin")
 beam.add_support(5, "roller")
-beam.add_point_load(20, 2)
-beam.analyze()
+beam.analyze_train(100)
 beam.display()
